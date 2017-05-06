086997c99ee949500ac20b17206d1edbf2e648f4
Sarbajit Saha <sarbajitsaha1@gmail.com>
2017-05-06 21:40:39 +0530

    Updated README

--- src/main.cpp
+++ src/main.cpp
@@ -7,21 +7,21 @@
 #include "chip8.h"
 
 uint8_t keymap[16] = {
-        SDLK_x,
+        SDLK_s,
         SDLK_1,
         SDLK_2,
         SDLK_3,
+        SDLK_4,
         SDLK_q,
         SDLK_w,
         SDLK_e,
-        SDLK_a,
-        SDLK_s,
-        SDLK_d,
-        SDLK_z,
-        SDLK_c,
-        SDLK_4,
         SDLK_r,
+        SDLK_a,
+        SDLK_d,
         SDLK_f,
+        SDLK_z,
+        SDLK_x,
+        SDLK_c,
         SDLK_v,
 };
 
